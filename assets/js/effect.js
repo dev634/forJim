e105ad1748ae97bacbc42bf07a4675a1f6f2cdc9
pablo1803 <pablolola@live.fr>
2020-06-17 18:54:47 +0200

    Init

---
+++ assets/js/effect.js
@@ -0,0 +1,24 @@
+let arrow1 = document.getElementById("arrow-1");
+let arrow2 = document.getElementById("arrow-2");
+let arrow3 = document.getElementById("arrow-3");
+let arrow4 = document.getElementById("arrow-4");
+let wrapper = document.getElementById("wrapper");
+
+arrow1.addEventListener("click", function (e) {
+	wrapper.classList.add("toLeft");
+});
+
+arrow2.addEventListener("click", function (e) {
+	wrapper.classList.add("toUp");
+});
+
+arrow3.addEventListener("click", function (e) {
+	wrapper.classList.add("toDown");
+	setTimeout(function () {
+		wrapper.classList.remove("toUp", "toLeft", "toRight", "toDown");
+	}, 500);
+});
+
+arrow4.addEventListener("click", function (e) {
+	wrapper.classList.add("toRight");
+});
